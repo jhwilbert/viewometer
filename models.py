dca0371e5e55b120e6ebae5c5e0c8fd12aa01907
paulskinner <paul.skinner@wk.com>
2011-09-29 14:58:20 +0100

    added new models

--- models.py
+++ models.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 from google.appengine.ext import db
 
+class SearchData(db.Model):
+    queryText           = db.TextProperty()
+    active              = db.BooleanProperty()
+    created             = db.DateTimeProperty(verbose_name=None, auto_now=False, auto_now_add=True)
+    lastQuery           = db.DateTimeProperty(verbose_name=None, auto_now=True)
+    videoReferenceList  = db.ListProperty()
+
 class VideoData(db.Model):
-    json            = db.TextProperty()
-    views           = db.TextProperty()
-    token           = db.TextProperty()
-    alertLevel		= db.TextProperty()
-    checkMeFlag     = db.BooleanProperty()
+    json                = db.TextProperty()
+    views               = db.TextProperty()
+    token               = db.TextProperty()
+    alertLevel	    	= db.TextProperty()
+    checkMeFlag         = db.BooleanProperty()
+    associatedSearch    = db.ReferenceProperty(SearchData)    
